8647cdeafd090dca2f8d7cc5dc0937e83b11675e
Muhammad Faiq Imran <faiqimran.fi@gmail.com>
2026-04-14 22:12:42 +0400

    Fixed some stuff

--- src/db.js
+++ src/db.js
@@ -3,15 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST     || '127.0.0.1',
-  user:     process.env.DB_USER     || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME     || 'ultimate_cad',
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 10
 });
 
 export default pool;
-
-// DB Structure
-//   
